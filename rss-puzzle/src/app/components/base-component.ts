5e6409a4c60e12dbebe7e64bf63ddab4593bab05
Julia <Y-1306@mail.ru>
2024-03-07 11:43:23 +0300

    feat: RSS_PR_00 Create Base Component (#162)
    * feat: add BaseComponent to the project
    
    * fix: pre-push husky

---
+++ rss-puzzle/src/app/components/base-component.ts
@@ -0,0 +1,87 @@
+export default class BaseComponent {
+  protected node: HTMLElement;
+
+  private children: BaseComponent[];
+
+  constructor({ tagName = 'div', className = '', textContent = '' }, ...children: BaseComponent[]) {
+    this.node = document.createElement(tagName);
+    this.node.className = className;
+    this.node.textContent = textContent;
+    this.children = children;
+
+    if (children) {
+      this.appendChildren(children);
+    }
+  }
+
+  public append(child: BaseComponent): void {
+    this.children.push(child);
+    this.node.append(child.getNode());
+  }
+
+  protected appendChildren(children: BaseComponent[]): void {
+    children.forEach((child) => {
+      this.append(child);
+    });
+  }
+
+  public getNode(): HTMLElement {
+    return this.node;
+  }
+
+  public getChildren(): BaseComponent[] {
+    return this.children;
+  }
+
+  public setAttribute(attribute: string, value: string): void {
+    this.node.setAttribute(attribute, value);
+  }
+
+  public setTextContent(content: string): void {
+    this.node.textContent = content;
+  }
+
+  public setHTML(html: string): void {
+    this.node.innerHTML = html;
+  }
+
+  public removeAttribute(attribute: string): void {
+    this.node.removeAttribute(attribute);
+  }
+
+  public addClass(className: string): void {
+    this.node.classList.add(className);
+  }
+
+  public toggleClass(className: string): void {
+    this.node.classList.toggle(className);
+  }
+
+  public addListener(
+    event: string,
+    listener: (e: Event) => void,
+    options: AddEventListenerOptions | boolean = false,
+  ): void {
+    this.node.addEventListener(event, listener, options);
+  }
+
+  public removeListener(
+    event: string,
+    listener: (e: Event) => void,
+    options: AddEventListenerOptions | boolean = false,
+  ): void {
+    this.node.removeEventListener(event, listener, options);
+  }
+
+  public destroyChildren(): void {
+    this.children.forEach((child) => {
+      child.destroy();
+    });
+    this.children.length = 0;
+  }
+
+  public destroy(): void {
+    this.destroyChildren();
+    this.node.remove();
+  }
+}
