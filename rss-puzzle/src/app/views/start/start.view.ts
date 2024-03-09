e1b761de93659cda70f73ed680793d404451f8bd
Julia <Y-1306@mail.ru>
2024-03-09 18:44:54 +0300

    feat: add display Greeting User (#167)

--- rss-puzzle/src/app/views/start/start.view.ts
+++ rss-puzzle/src/app/views/start/start.view.ts
@@ -1,5 +1,6 @@
 import './start.view.css';
 import BaseComponent from '../../components/base-component';
+import { LocalStorageServise } from '../../servises/local-storage.servise';
 
 export default class StartView extends BaseComponent {
   constructor() {
@@ -15,6 +16,30 @@ export default class StartView extends BaseComponent {
       textContent:
         'Collect puzzles and learn English! Make unique offers and discover amazing pictures! Click and drag small pieces onto the field and gradually reveal fragments of the picture. If you encounter any difficulties, use the hints. Enjoy relaxing puzzles to train your brain and improve your language level!',
     });
-    this.appendChildren([pageTitle, pageText]);
+    const stiker = this.createSicker();
+    this.appendChildren([pageTitle, pageText, stiker]);
+  }
+
+  private createSicker(): BaseComponent {
+    return new BaseComponent(
+      {
+        tagName: 'div',
+        className: 'page-sticker',
+      },
+      new BaseComponent({
+        tagName: 'span',
+        className: 'page-sticker-text',
+        textContent: this.createGreeting(),
+      }),
+    );
+  }
+
+  private createGreeting(): string {
+    const user = LocalStorageServise.getUser('user');
+    console.log(user);
+    if (user) {
+      return `Hello, ${user.name} ${user.surname}!`;
+    }
+    return '';
   }
 }
